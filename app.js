c760ebe60c4594c2b1a67966d5a3261f74649b0b
iolado <oladokui@oregonstate.edu>
2026-02-21 13:07:56 -0800

    MW

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ import express from "express";
 const app = express();
 export default app;
 
-import employees from "#db/employees";
+import employees from "#api/employees";
+
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello employees!");
+});
+
+app.use("/employees", employeesRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send("Sorry, something went wrong :(");
 });
 
 app.get("/employees", (req, res) => {
